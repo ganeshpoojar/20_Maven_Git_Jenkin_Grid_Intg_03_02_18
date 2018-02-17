841d7531c499d468b4ad450175f05040f36d0ba3
unknown <ganeshpoojar@gmail.com>
2018-02-17 17:24:57 +0530

    Second message

--- src/pack1/Test1.java
+++ src/pack1/Test1.java
@@ -1,16 +1,32 @@
 package pack1;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Test1 {
+	
+	public Logger log;
+	public  ChromeDriver driver;
+	
 	static{
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 	}
-  @Test
-  public void Demo() {
-	  ChromeDriver driver = new ChromeDriver();
-	  driver.get("http://www.google.com");
+	
+	@BeforeClass
+	public void log4j(){
+	Logger.getLogger(getClass());			  
+	}
+	
+	
+	@Test
+	public void Demo() {	
+	  log.setLevel(Level.INFO);	 	  
+	  new ChromeDriver();
+	  driver.get("http://www.google.com");  	 
+	  log.info("Hello");
 	  driver.quit();
   }
 }
